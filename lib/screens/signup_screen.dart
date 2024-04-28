12e10094218b957d911d85ead1701b8c39a1ca0d
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2024-04-28 16:44:55 +0530

    Fixed Scroll Issue

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/resources/auth_method.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
@@ -33,109 +34,146 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: SafeArea(
           child: Container(
         padding: EdgeInsets.symmetric(horizontal: 32),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Flexible(
-              child: Container(),
-              flex: 2,
-            ),
-
-            //svg image
-            SvgPicture.asset(
-              'assets/images/ic_instagram.svg',
-              color: primaryColor,
-              height: 64,
-            ),
-            SizedBox(
-              height: 64,
-            ),
-            // text field for username
-            TextFieldInput(
-              hintText: 'Enter your username',
-              textInputType: TextInputType.text,
-              textEditingController: _usernameController,
-            ),
-            SizedBox(
-              height: 24,
-            ),
-
-            //text field for email
-
-            TextFieldInput(
-              hintText: 'Enter Your Email',
-              textInputType: TextInputType.emailAddress,
-              textEditingController: _emailController,
-            ),
-
-            SizedBox(
-              height: 24,
-            ),
-            // text field for password
-            TextFieldInput(
-              hintText: 'Enter Your Password',
-              textInputType: TextInputType.text,
-              textEditingController: _passwordController,
-              isPass: true,
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            // Bio
-            TextFieldInput(
-              hintText: 'Enter your Bio',
-              textInputType: TextInputType.text,
-              textEditingController: _bioController,
-            ),
-            SizedBox(
-              height: 24,
-            ),
-
-            // login button
-            InkWell(
-              child: Container(
-                child: const Text('Log In'),
-                width: double.infinity,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                decoration: ShapeDecoration(
-                  color: blueColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Flexible(
-              child: Container(),
-              flex: 2,
-            ),
-
-            // Transition to Signing up
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height -
+                MediaQuery.of(context).padding.top,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
-                  child: Text("Don't have an account?"),
-                  padding: const EdgeInsets.symmetric(vertical: 8),
+                Flexible(
+                  child: Container(),
+                  flex: 1,
                 ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    child: Text(
-                      "Sign up.",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+
+                //svg image
+                SvgPicture.asset(
+                  'assets/images/ic_instagram.svg',
+                  color: primaryColor,
+                  height: 64,
+                ),
+                SizedBox(
+                  height: 64,
+                ),
+
+                //Circular Widget to accept and show our selcted file
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 64,
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1591779051696-1c3fa1469a79?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    Positioned(
+                      bottom: -10,
+                      left: 80,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.add_a_photo),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                // text field for username
+                TextFieldInput(
+                  hintText: 'Enter your username',
+                  textInputType: TextInputType.text,
+                  textEditingController: _usernameController,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+
+                //text field for email
+
+                TextFieldInput(
+                  hintText: 'Enter Your Email',
+                  textInputType: TextInputType.emailAddress,
+                  textEditingController: _emailController,
+                ),
+
+                SizedBox(
+                  height: 24,
+                ),
+                // text field for password
+                TextFieldInput(
+                  hintText: 'Enter Your Password',
+                  textInputType: TextInputType.text,
+                  textEditingController: _passwordController,
+                  isPass: true,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                // Bio
+                TextFieldInput(
+                  hintText: 'Enter your Bio',
+                  textInputType: TextInputType.text,
+                  textEditingController: _bioController,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+
+                // login button
+                InkWell(
+                  child: Container(
+                    child: const Text('Log In'),
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    decoration: ShapeDecoration(
+                      color: blueColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
                   ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Flexible(
+                  child: Container(),
+                  flex: 2,
+                ),
+
+                // Transition to Signing up
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Text("Don't have an account?"),
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                    ),
+                    GestureDetector(
+                      onTap: () async {
+                        String res = AuthMethods()
+                            .signUpUser(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                                username: _usernameController.text,
+                                bio: _bioController.text)
+                            .toString();
+                        print(res);
+                      },
+                      child: Container(
+                        child: Text(
+                          "Sign up.",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       )),
     );
